a96ddb34a8dd434d012becd32a7ecb22bf4ecaed
2364058719 <72687683+2364058719@users.noreply.github.com>
2024-05-26 09:36:00 +0800

    Initial commit

---
+++ src/main/java/com/awei/App.java
@@ -0,0 +1,13 @@
+package com.awei;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
